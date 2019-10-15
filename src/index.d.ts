f41105be044e88b2ee3d4b10633a34cc1d8f3ba8
Andrew Thomas <andrewmthomas87@gmail.com>
2019-10-15 16:33:27 -0500

    Exports BlocKey

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 import Bloc from './Bloc';
 import BlocBuilder from './BlocBuilder';
-import container from './container';
+import container, { BlocKey } from './container';
 import { useBlocState, useBlocDerivedState } from './hooks';
-export { Bloc, BlocBuilder, container, useBlocState, useBlocDerivedState };
+export { Bloc, BlocBuilder, container, BlocKey, useBlocState, useBlocDerivedState };
